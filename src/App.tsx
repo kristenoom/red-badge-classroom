77e8adf348de8a0261b40f4d3ec38437de713c5b
Kristen Oom <kris.a.oom@gmail.com>
2021-10-07 19:30:38 -0400

    stateExample

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import PropsExample from "./components/PropsExample.jsx";
-import PropsMapping from './components/PropsMapping.jsx';
+import StateExample from "./components/StateExample.jsx";
 
 function App() {
-    const visitedPlaces = ["California", "Connecticut", "Florida", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Michigan", "Minnesota", "Missouri", "Nevada", "North Carolina", "North Dakota", "Ohio", "Pennsylvania", "South Dakota", "Tennessee", "Texas", "West Virginia", "Wisconsin" ];
     return (
         <div>
-            <PropsExample name={"Tom"} businessName={"MySpace"} />
-            <PropsExample name={"Mark"} businessName={"Facebook"} />
-            
-            <PropsMapping title={"Places I visited"} visitedPlaces={visitedPlaces}/>
+            <StateExample />
         </div>
     );
 }
--- src/components/StateExample.jsx
+++ src/components/StateExample.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 class StateExample extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { currentTemp: 71, cloudCoverage: "overcast" };
+
+    }
+    handleClick = () => {
+        this.setState({ currentTemp: 48 })
+    };
+
     render() {
         return (
             <div>
                 <h1>Hello from State Example</h1>
+                <p>The current temp is {this.state.currentTemp}</p>
+                <p>The cloud coverage is {this.state.cloudCoverage}</p>
+                <button onClick={this.handleClick}>Click to Change Temp</button>
             </div>
         )
     }
